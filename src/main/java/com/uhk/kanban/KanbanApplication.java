a5804a85e40c28efc743a46f1e2052d200106548
ContiHan <daniel.hanak@outlook.cz>
2025-04-13 20:51:31 +0200

    git init

--- src/main/java/com/uhk/kanban/KanbanApplication.java
+++ src/main/java/com/uhk/kanban/KanbanApplication.java
@@ -2,7 +2,6 @@ package com.uhk.kanban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
 
 @SpringBootApplication
 public class KanbanApplication {
